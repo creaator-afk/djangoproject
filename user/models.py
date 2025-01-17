86bee3bff65b7c5d035cd3671d2e3bd48977d78b
Kshitij Satpathy <kshitij.satpathy@zopper.com>
2025-01-17 15:01:00 +0530

    test2

--- user/models.py
+++ user/models.py
@@ -1,10 +1,29 @@
+# user/models.py
+
 from django.db import models
 
-# Create your models here.
-
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+class Plan(models.Model):
+    PLAN_CHOICES = [
+        ('Child Plan', 'Child Plan'),
+        ('Young Adult Plan', 'Young Adult Plan'),
+        ('Adult Plan', 'Adult Plan'),
+        ('Senior Plan', 'Senior Plan'),
+    ]
+
+    user = models.ManyToManyField(User, related_name='plans')
+    plan_type = models.CharField(max_length=50, choices=PLAN_CHOICES)
+    from_days = models.DateField(null=True, blank=True)
+    to_days = models.DateField(null=True, blank=True)
+    from_year = models.IntegerField(null=True, blank=True)
+    to_year = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.plan_type} for {self.user.name}"
